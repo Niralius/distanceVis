24ed7b09403e09aacaf832fa933bdbfe0f677ae9
Niralius <michaelheffnercruz@gmail.com>
2016-05-05 11:08:49 -0400

    Push from PC

--- MockupDV/src/mockupdv/DistanceChooser.java
+++ MockupDV/src/mockupdv/DistanceChooser.java
@@ -1,44 +1,61 @@
+package mockupdv;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mockupdv;
-
-import java.util.List;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 /**
  *
- * @author MichaelH
+ * @author santi
  */
 public class DistanceChooser {
     
-    public ArrayList<String> pos = new ArrayList();
-    public ArrayList<String> labels = new ArrayList();
-    public ArrayList<String> xPos = new ArrayList();
-    public ArrayList<String> yPos = new ArrayList();
-    public ArrayList<String> zPos = new ArrayList();
-    
-    DistanceChooser(String file) throws IOException{
+    public DistanceChooser(String file) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader("/Users/Michael/Documents/1.txt"));
         
-        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-            // process the line.
-            StringTokenizer stk = new StringTokenizer(line, "\t");
-            labels.add(stk.nextToken());
-            xPos.add(stk.nextToken());
-            yPos.add(stk.nextToken());
-            zPos.add(stk.nextToken());
-            
+        boolean first = true;
+        int sizeOfMatrix = 0;
+        List<String> ids = new ArrayList<>();
+        double m[][] = null;
+        int i = 0;
+        while(true) {
+            String line = br.readLine();
+            if (line==null) break;
+            if (first) {
+                StringTokenizer st = new StringTokenizer(line," \t");
+                st.nextToken();
+                while(st.hasMoreTokens()) {
+                    String t = st.nextToken();
+                    sizeOfMatrix++;
+                }
+                m = new double[sizeOfMatrix][sizeOfMatrix];
+                first = false;
             }
+
+            StringTokenizer st = new StringTokenizer(line," \t");
+            int j = 0;
+            String id = st.nextToken();
+            ids.add(id);
+            while(st.hasMoreTokens()) {
+                String t = st.nextToken();
+                m[i][j] = Double.parseDouble(t);
+                j++;
+            }
+            i++;
         }
+
+        System.out.println(ids);
         
     }
 }
+
+
+
